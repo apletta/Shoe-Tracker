408e5faa2323a429acbeded043fe514c333a2952
Your Name <jmays2@wisc.edu>
2019-04-24 17:47:39 -0500

    J

--- application/Main.java
+++ application/Main.java
@@ -21,11 +21,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-<<<<<<< HEAD
 	        Scene scene = new Scene(ProductInfoScreen.screen(),1600,900);
-=======
-	        Scene scene = new Scene(HomeScreen.screen(),1600,900);
->>>>>>> ba521ae045bb18151db6222b69a02dafaa371d7e
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Sole Table");
 			primaryStage.setScene(scene);
