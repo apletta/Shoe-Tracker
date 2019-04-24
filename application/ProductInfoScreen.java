ba521ae045bb18151db6222b69a02dafaa371d7e
Alex Pletta <apletta@wisc.edu>
2019-04-24 15:15:56 -0500

    added DeleteProductScreen transition

--- application/ProductInfoScreen.java
+++ application/ProductInfoScreen.java
@@ -1,6 +1,8 @@
 package application;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.*;
 import javafx.stage.*;
 import javafx.scene.*;
@@ -59,6 +61,22 @@ class ProductInfoScreen {
 	  checkSize.setAlignment(Pos.CENTER);
 	  Button delete = new Button("Delete Product(s)");
 	  delete.setAlignment(Pos.CENTER);
+
+		// delete actions
+		delete.setOnAction(new EventHandler<ActionEvent>() {
+
+			public void handle(ActionEvent event) {
+				// Load ProductInfoScreen
+				Stage stage = (Stage) delete.getScene().getWindow();
+				Scene scene = new Scene(DeleteProductScreen.screen(), 1600, 900);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				stage.setTitle("Sole Table");
+				stage.setScene(scene);
+				stage.hide();
+				stage.show();
+			}
+		});
+
 	  
 	  VBox vbox = new VBox(15);
 	  vbox.setAlignment(Pos.CENTER);
