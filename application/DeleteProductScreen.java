f70c1fa6164575831644a5079f30d749e64d6988
Your Name <jmays2@wisc.edu>
2019-04-23 22:11:17 -0500

    add DeleteProductScreen.java

---
+++ application/DeleteProductScreen.java
@@ -0,0 +1,55 @@
+package application;
+
+class DeleteProductScreen {
+
+
+	protected static BorderPane screen() {
+	  ///Everything put into VBox
+	  //1. Need a GridPane with "size" text and 
+	  //   size choices(use HBOX)
+	  //2. Need HBox for buttons
+	  GridPane pane = new GridPane();
+	  pane.setHgap(5);
+	  pane.setVgap(5);
+	  pane.setPadding(new Insets(10,10,10,10));
+	  
+	  Text txt = new Text("Size");
+	  txt.setStyle("-fx-font-weight: bold");
+	  GridPane.setConstraints(txt, 0, 0);
+	  pane.setAlignment(Pos.CENTER);
+	  
+	  HBox box = new HBox(9);
+	  for(double i = 9; i < 13; i = i + 0.5) {
+	    Button newButt = new Button(i+"");
+	    newButt.setAlignment(Pos.CENTER);
+	    newButt.setId("");
+	    box.getChildren().add(newButt);
+	  }
+	  box.setAlignment(Pos.CENTER);
+	  GridPane.setConstraints(box, 0, 1);
+	  pane.getChildren().addAll(txt,box);
+	  
+	  HBox hbox = new HBox(1);
+	  ComboBox<Integer> combo = new ComboBox<>();
+	  combo.setId("Quan");
+      for(int i = 1; i < 11; i++) {
+        combo.getItems().add(i);
+      }
+      
+      Button button = new Button("DELETE");
+      button.setAlignment(Pos.CENTER);
+      hbox.setAlignment(Pos.CENTER);
+      hbox.getChildren().addAll(combo,button);
+      
+      VBox vbox = new VBox(15);
+      vbox.setAlignment(Pos.CENTER);
+      vbox.getChildren().addAll(pane,hbox);
+      
+	  
+	  BorderPane borderPane = new BorderPane();
+	  borderPane.setCenter(vbox);
+	  borderPane.setTop(topArea("Delete Product"));
+	  return borderPane;
+	}
+
+}
--- application/ProductInfoScreen.java
+++ application/ProductInfoScreen.java
@@ -15,45 +15,69 @@ import application.TopArea.*;
 class ProductInfoScreen {
 	
       protected static BorderPane screen() {
-        GridPane pane = new GridPane();
-        pane.setHgap(10);
-        pane.setVgap(10);
-        pane.setPadding(new Insets(10,10,10,10));
-        
-        //Rectangle showView = new Rectangle();
-        
-        Text prodNum = new Text("Product #:");
-        GridPane.setConstraints(prodNum, 0, 0);
-        prodNum.setId("text");
-        
-        Text displayNum = new Text("This is the product num");
-        displayNum.setId("text2");
-        GridPane.setConstraints(displayNum, 1, 0);
-        
-        Text name = new Text("Name:");
-        GridPane.setConstraints(name, 0, 1);
-        name.setId("text");
-        Text displayName = new Text("This is the product name");
-        displayName.setId("text2");
-        GridPane.setConstraints(displayName, 1, 1);
-        
-        Text amountInStock = new Text("Amount in Stock:");
-        GridPane.setConstraints(amountInStock, 0, 2);
-        amountInStock.setId("text");
-        Text displayStock = new Text("This is the amount in stock");
-        displayStock.setId("text2");
-        GridPane.setConstraints(displayStock, 1, 2);
-        
-        pane.setAlignment(Pos.CENTER);
-        pane.getChildren().addAll(prodNum,name,amountInStock,displayNum,
-            displayName,displayStock);
-        
-        BorderPane root = new BorderPane();
-        root.setCenter(pane);
-        root.setTop(TopArea.topArea("Product Info"));
-        
-        return root;
-      }
-
+	  GridPane pane = new GridPane();
+	  pane.setHgap(10);
+	  pane.setVgap(10);
+	  pane.setPadding(new Insets(10,10,10,10));
+	  
+	  ImageView image = new ImageView(new Image("aj1.png"));
+	  image.setFitWidth(200);
+	  image.setFitHeight(150);
+	  
+	  Text prodNum = new Text("Product #:");
+	  GridPane.setConstraints(prodNum, 0, 0);
+	  prodNum.setId("text");
+	  
+	  Text displayNum = new Text("This is the product num");
+	  displayNum.setId("text2");
+	  GridPane.setConstraints(displayNum, 1, 0);
+	  
+	  Text name = new Text("Name:");
+	  GridPane.setConstraints(name, 0, 1);
+	  name.setId("text");
+	  Text displayName = new Text("This is the product name");
+	  displayName.setId("text2");
+	  GridPane.setConstraints(displayName, 1, 1);
+	  
+	  Text amountInStock = new Text("Amount in Stock:");
+	  GridPane.setConstraints(amountInStock, 0, 2);
+	  amountInStock.setId("text");
+	  Text displayStock = new Text("This is the amount in stock");
+	  displayStock.setId("text2");
+	  GridPane.setConstraints(displayStock, 1, 2);
+	  
+	  pane.setAlignment(Pos.CENTER);
+	  pane.getChildren().addAll(prodNum,name,amountInStock,displayNum,
+	      displayName,displayStock);
+	  
+	  HBox hbox = new HBox(30);
+	  hbox.setAlignment(Pos.CENTER);
+	  hbox.getChildren().addAll(image,pane);
+	  
+	  Button checkSize = new Button("Check Sizes");
+	  checkSize.setAlignment(Pos.CENTER);
+	  Button delete = new Button("Delete Product(s)");
+	  delete.setAlignment(Pos.CENTER);
+	  
+	  VBox vbox = new VBox(15);
+	  vbox.setAlignment(Pos.CENTER);
+	  vbox.getChildren().addAll(delete, checkSize);
+	  
+	  BorderPane.setAlignment(delete, Pos.BOTTOM_LEFT);
+	  delete.setAlignment(Pos.CENTER);
+	  
+	  VBox box = new VBox(80);
+	  box.setAlignment(Pos.CENTER);
+	  box.getChildren().addAll(hbox,vbox);
+	  
+	  
+	  
+	  BorderPane root = new BorderPane();
+	  root.setCenter(box);
+	  root.setTop(TopArea.topArea("Product Info"));
+	  
+	  
+	  return root;
+	}
 
 }
