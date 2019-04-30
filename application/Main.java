f7b144fd4c7f43a3b934c999f82120ce0720fd7b
Alex Pletta <apletta@wisc.edu>
2019-04-30 18:50:02 -0500

    removed test lines

--- application/Main.java
+++ application/Main.java
@@ -26,10 +26,6 @@ public class Main extends Application {
 	        Scene scene = new Scene(HomeScreen.screen(),1600,900);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Sole Table");
-			// test test test
-			
-			// testing stash
-			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
